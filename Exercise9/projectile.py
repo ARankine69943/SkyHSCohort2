06b51df672e17dd002e89bff592ee30d887a477d
Aeoleon876456 <Aeoleon.rankine@sky.uk>
2023-02-13 19:22:31 +0000

    Updated Exercise 9

--- Exercise9/projectile.py
+++ Exercise9/projectile.py
@@ -5,10 +5,11 @@ k = 1.3962
 x = 0.5
 x_squared = x**2
 yo = 1
-#
+
 #print (math.cos(k))
 #print (math.tan(k))
-#
+
 y = yo + x * (math.tan(k)) - ((g * x ** 2) / (2 * (vo * (math.cos(k))) ** 2))
 print(y)
+
 # #theta = 80 * (math.pi/180) #0.0174 #(1.3962)
